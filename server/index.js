af0842cabec9f0556006c08e883f114873e98d16
FarahShiba <tambunanfarahshiba@gmail.com>
2024-12-02 14:46:19 +1100

    done

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-const { ApolloServer } = require("@apollo/server");
+const { ApolloServer } = require("@apollo/server"); //import from Apollo server
 const { startStandaloneServer } = require("@apollo/server/standalone");
 // const { startServerAndCreateNextHandler } = require("@as-integrations/next");
 const glob = require("glob");
